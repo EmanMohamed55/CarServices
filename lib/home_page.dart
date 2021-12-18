d01d8c3fd4a6ff5141c1c8724ebd9d89bb29f6f4
AhmedElsayed <ae234918@gmail.com>
2021-12-18 10:45:52 +0200

    start screen

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class homepage extends StatelessWidget{
+  static const String routeName='home';
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width:double.infinity,
+      color: Colors.cyan,
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+
+import 'package:car_zone/home_page.dart';
+import 'package:car_zone/login_screen.dart';
+import 'package:car_zone/sgin_in_screen.dart';
+import 'package:car_zone/sochial.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title:'Car Zone',
+       routes: {
+          logincreen.routeName:(buildcontext)=>logincreen(),
+         sginin.routeName:(buildcontext)=>sginin(),
+         homepage.routeName:(buildcontext)=>homepage(),
+         SignInDemo.routeName:(buildcontext)=>SignInDemo(),
+
+       },
+        initialRoute: logincreen.routeName,
+      );
+  }
+}
+
